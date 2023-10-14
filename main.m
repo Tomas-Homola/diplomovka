a8b54c5548d0505b3b544634631e8b73f84fea75
Tomas-Homola <homola.tom6@gmail.com>
2023-10-15 01:07:02 +0200

    Testovanie nových tried dataPreprocessor a dataFeatureExtractor

--- main.m
+++ main.m
@@ -7,12 +7,13 @@ clc;
 
 % download: https://drive.google.com/drive/folders/1EWlkqu3qofB5kB5QLcPkrFyJxFm2zBec?usp=sharing
 DATA_DIRECTORY = fullfile(MAIN_DIRECTORY, 'data');
-DTM_DIRECOTRY  = fullfile(DATA_DIRECTORY, 'ExportDMR_lesBodiky');
-PC_DIRECTORY   = fullfile(DATA_DIRECTORY, 'ExportMB_lesBodiky');
+DTM_DIRECTORY  = fullfile(DATA_DIRECTORY, 'ExportDMR');
+PC_DIRECTORY   = fullfile(DATA_DIRECTORY, 'ExportMB');
 
 cd(MAIN_DIRECTORY);
 
-%% COLOR MAP
+addpath(MAIN_DIRECTORY);
+
 colorMap = load('las_colormap.txt')/255; % color map for classified point cloud
 
 COLOR_GROUND     = colorMap(2,:);
@@ -21,21 +22,21 @@ COLOR_VEG_MEDIUM = colorMap(4,:);
 COLOR_VEG_HIGH   = colorMap(5,:);
 
 %% DATA READING - DMR
-cd(DTM_DIRECOTRY);
+cd(DTM_DIRECTORY);
 
-[heightMatrix, rasterReference] = readgeoraster('dmr.tif', 'OutputType', 'double');
+[DTM, rasterReference] = readgeoraster('dmr.tif', 'OutputType', 'double');
 infoDMR = georasterinfo('dmr.tif');
 
-heightMatrix = standardizeMissing(heightMatrix, infoDMR.MissingDataIndicator);
-heightMatrix = flipud(heightMatrix);
+DTM = standardizeMissing(DTM, infoDMR.MissingDataIndicator);
+DTM = flipud(DTM);
 rasterReference.ColumnsStartFrom = 'south';
 
 %% PLOT - DMR
 figure('Name', 'Digital Terrain Model data')
 title 'DTM data'
-mapshow(heightMatrix, rasterReference, 'DisplayType', 'surface');
-demcmap(heightMatrix, 256)
-view(2);
+mapshow(DTM, rasterReference, 'DisplayType', 'surface');
+demcmap(DTM, 256)
+view(3);
 daspect([1 1 1]) % cim je posledne cislo blizsie ku 0, tym viac sa skaluje "z" suradnica
 axis normal
 
@@ -48,11 +49,17 @@ ptCloud = cell(lasCount, 1);
 ptAttributes = cell(lasCount, 1);
 colorData = cell(lasCount, 1);
 
+nPoints = 0;
+
 for i = 1:lasCount
     lasReader = lasFileReader(lasFiles(i).name);
     [ptCloud{i}, ptAttributes{i}] = readPointCloud(lasReader, 'Attributes', 'Classification');
     colorData{i} = reshape(label2rgb(ptAttributes{i}.Classification, colorMap, 'k'), [], 3);
+	nPoints = nPoints + ptCloud{i}.Count;
 end
+
+% average number of points
+fprintf("Average point count per pixel: %.2f\n", nPoints / nnz(~isnan(DTM(:))));
 
 %% PLOT - PC
 % 2 -> Ground
@@ -75,58 +82,78 @@ end
 hold off
 
 %%
+% cd(MAIN_DIRECTORY);
+% 
+% handle = dataHandler(DTM, rasterReference, ptCloud, ptAttributes);
+% handle = handle.meshPlane();
+% handle = handle.computePointCloudAttributes();
+% handle = handle.normalizePtCloud("method","DTM");
+
+%% Data preprocessor
 cd(MAIN_DIRECTORY);
 
-handle = dataHandler(heightMatrix, rasterReference, ptCloud, ptAttributes);
-handle = handle.meshPlane();
-handle = handle.computePointCloudAttributes();
-handle = handle.normalizePtCloud("method","DTM");
+preprocessor = dataPreprocessor(DTM, rasterReference, ptCloud, ptAttributes);
+preprocessor = preprocessor.meshPlane();
+preprocessor = preprocessor.filterPointCloud();
+preprocessor = preprocessor.normalizePtCloud("method","DTM");
 
 %% PLOT ORIGINAL AND NORMALIZED POINT CLOUD
 figure("Name","Original Point Cloud")
-handle.plotPtCloud3D(colorMap, [2, 3, 4, 5],"useData","original")
-
+preprocessor.plotPtCloud3D(colorMap, [2, 3, 4, 5],"useData","original")
+%%
 figure("Name","Normalized Point Cloud")
-handle.plotPtCloud3D(colorMap, [2, 3, 4, 5],"useData","normalized")
+preprocessor.plotPtCloud3D(colorMap, [2, 3, 4, 5],"useData","normalized")
+% handle.plotPtCloud3D(colorMap, [5],"useData","normalized")
 
 %% COMPUTE ALL METRICS FOR GIVEN POINT CLOUD
-handle = handle.computeMetricRasters();
+% handle = handle.computeMetricRasters();
+
+featureExtractor = dataFeatureExtractor(rasterReference,... % original RasterReference
+										preprocessor.ptCloud_norm,... % normalized point cloud
+										preprocessor.ptAttributes_norm,... % and its attributes
+										2); % desired pixel size in meters
+
+featureExtractor = featureExtractor.meshPlane();
+featureExtractor = featureExtractor.computeMetricRasters();
 
 %% PLOT SELECTED METRIC
 figure
-handle.plotMetricRaster("plotData","PPR")
-% colormap jet
-colormap hot
+% handle.plotMetricRaster("plotData","Hmax")
+featureExtractor.plotMetricRaster("plotData","Hmax")
+colormap jet
+% colormap hot
 colorbar
 axis equal
 axis xy
 
 %%
+handle.plotMetricRasters();
+%%
 figure
-imagesc([handle.x1 handle.x2], [handle.y1 handle.y2], temp,...
+imagesc([handle.x1 handle.x2], [handle.y1 handle.y2], temp2,...
 	'AlphaData', handle.alphaData_DTM)
-title 'Max of normalized height'
+title '99th perc'
 colormap hot
 colorbar
 axis equal
 axis xy
 
 %% EXPORT SELECTED METRIC TO .TIF FILE
-handle.exportMetricRaster("data_lesSipka","exportLayer","Hstd");
+% handle.exportMetricRaster("data_lesSipka","exportLayer","Hstd");
 
 %% EXPORT ALL METRICS TO .TIF FILES
-handle.exportAllMetricRasters("data_lesBodiky");
+handle.exportAllMetricRasters("data_lesBodiky2");
 
 %%
-[H, R] = readgeoraster('Hmax.tif', 'OutputType', 'double');
+% [H, R] = readgeoraster('Hmax.tif', 'OutputType', 'double');
 
-H = flipud(H);
+% H = flipud(H);
 
 %% Export color palette
-c = jet;
-c = c*255;
-writematrix(c, "jet_palette.txt","Delimiter",' ');
-
+% c = jet;
+% c = c*255;
+% writematrix(c, "jet_palette.txt","Delimiter",' ');
+% 
 
 
 
